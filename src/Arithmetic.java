2881f9629fc17bf2be60138d4517154b76dfa921
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2025-03-17 20:31:52 +0200

    Update Arithmetic class to demonstrate division operation

--- src/Arithmetic.java
+++ src/Arithmetic.java
@@ -1,11 +1,16 @@
 public class Arithmetic {
 
     public static void main(String[] args) {
-        int x  = 10;
-        int y = 2;
-        int z;
+        int x = 10;
+        // int x = 10;
+        double y = 3;
+        double z;
 
-        z = x - y;
+        // z = x - y;
+        // z = x + y;
+        // z = x * y;
+        // z = x % y;
+        z = x / y;
 
         System.out.println(z);
     }
