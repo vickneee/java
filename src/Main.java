f33284fef106e88c7bfa7322181a0d2d01f1b1f1
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2025-03-17 12:19:07 +0200

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,53 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        // This is a comment
+        /* This is a multiline
+        comment */
+
+        System.out.println("I like pizza!");
+        System.out.println("I like pizza too!");
+        System.out.print("I like pizza too!\n");
+        System.out.println("I like pizza too! cm");
+
+        int myNum = 5;
+        System.out.println(myNum);
+
+        int age = 21;
+        System.out.println("I am " + age + " years old.");
+
+        double myDouble = 3.14;
+        System.out.println(myDouble);
+
+        char myLetter = 'D';
+        System.out.println(myLetter);
+
+        String myText = "Victoria";
+        System.out.println(myText);
+
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Please enter your age: ");
+        int age2 = scanner.nextInt();
+
+        System.out.println("Please enter your height: ");
+        double height = scanner.nextDouble();
+
+        System.out.println("Are you a Student? (true/false): ");
+        boolean isStudent = scanner.nextBoolean();
+
+        System.out.println("Your name is: " + name + " and your age is: " + age2 + " and your height is: " + height);
+
+        if (isStudent) {
+            System.out.println("You are a student.");
+        } else {
+            System.out.println("You are NOT a student.");
+        }
+
+        // Mad Libs Game
 
         String adjective1;
         String noun1;
