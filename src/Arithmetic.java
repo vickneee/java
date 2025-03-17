c652cc234860817c6ea288c14323c51a1e2ccc00
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2025-03-17 18:42:33 +0200

    Add Arithmetic class with basic subtraction functionality

---
+++ src/Arithmetic.java
@@ -0,0 +1,12 @@
+public class Arithmetic {
+
+    public static void main(String[] args) {
+        int x  = 10;
+        int y = 2;
+        int z;
+
+        z = x - y;
+
+        System.out.println(z);
+    }
+}
