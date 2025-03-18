9f22f8a4b5e265a1f355d4d1aa1bcffa81feb1e0
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2025-03-18 21:21:39 +0200

    Initial commit

--- switch/src/SwitchCase.java
+++ switch/src/SwitchCase.java
@@ -1,7 +1,26 @@
+import java.util.Scanner;
+
 public class SwitchCase {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Give a number: ");
+        int number = Integer.parseInt(scanner.nextLine());
+
+        switch (number) {
+            case 1:
+                System.out.println("The number is one.");
+                break;
+            case 2:
+                System.out.println("The number is two.");
+                break;
+            default:
+                System.out.println("This is a default value!");
+                break;
+        }
+
+        scanner.close();
     }
 }
