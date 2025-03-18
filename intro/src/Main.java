f930830042b8cadeddf09b7a1cc37f8e9454f4f3
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2025-03-18 21:08:40 +0200

    Initial commit

--- intro/src/Main.java
+++ intro/src/Main.java
@@ -51,31 +51,6 @@ public class Main {
             System.out.println("You are NOT a student.");
         }
 
-        // Mad Libs Game
-        System.out.println("Let's play a Mad Libs game!");
-
-        String adjective1;
-        String noun1;
-        String adjective2;
-        String verb1;
-        String adjective3;
-
-        System.out.println("Enter an adjective (description): ");
-        adjective1 = scanner.nextLine();
-        System.out.println("Enter a noun (animal or person): ");
-        noun1 = scanner.nextLine();
-        System.out.println("Enter an adjective (description): ");
-        adjective2 = scanner.nextLine();
-        System.out.println("Enter a verb end with -ing (action): ");
-        verb1 = scanner.nextLine();
-        System.out.println("Enter an adjective (description): ");
-        adjective3 = scanner.nextLine();
-
-        System.out.println("Today I went to the " + adjective1 + " zoo.");
-        System.out.println("In an exhibit, I saw a " + noun1 + ".");
-        System.out.println(noun1 +" was " + adjective2 + " and " + verb1 + "!");
-        System.out.println("I was a " + adjective3 + "!");
-
         // Close the scanner
         scanner.close();
     }
