93f02749dc6e562d8fa742a8101d8a81fe96852a
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2025-03-18 21:43:40 +0200

    Initial commit

---
+++ random/src/RandomNumber.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+
+public class RandomNumber {
+
+    public static void main(String[] args) {
+
+        Random random = new Random();
+
+        int number;
+        double numberExample;
+
+        numberExample = random.nextDouble(); // Random number between 0 and 1
+
+        number = random.nextInt(1, 101);
+
+        System.out.println(number);
+        System.out.println(numberExample);
+
+        boolean isHeads;
+
+        isHeads = random.nextBoolean();
+
+        if (isHeads) {
+            System.out.println("HEADS");
+        }
+        else {
+            System.out.println("TAILS");
+        }
+    }
+}
