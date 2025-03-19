c279983bca35efcdd1d7a6ae561a9103bba3cd07
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2025-03-19 19:57:02 +0200

    Initial commit

---
+++ nested-loops/src/NestedLoops.java
@@ -0,0 +1,19 @@
+public class NestedLoops {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++) {
+            for ( int j = 1; j <= 5; j++) {
+
+                int product = i * j;
+                System.out.print(i + " * " + j + " = " + product + " ");
+
+                if (product % 2 == 0) {
+                    System.out.println("Even!");
+                } else {
+                    System.out.println("Odd!");
+                }
+            }
+        }
+    }
+}
