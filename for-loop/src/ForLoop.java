f9f31c4299833b7519b528fb1503708072170dfd
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2025-03-21 18:37:28 +0200

    Initial commit

---
+++ for-loop/src/ForLoop.java
@@ -0,0 +1,17 @@
+public class ForLoop {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10 ; i++) {
+            System.out.println(i);
+        }
+
+        System.out.println("Loop ended.");
+
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+        }
+
+        System.out.println("Loop ended.");
+    }
+}
