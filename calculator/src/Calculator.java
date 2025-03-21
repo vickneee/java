90d95727f323f7e8d3ec9e94f266d769565ae61b
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2025-03-21 14:57:45 +0200

    Initial commit

--- calculator/src/Calculator.java
+++ calculator/src/Calculator.java
@@ -6,28 +6,45 @@ public class Calculator {
 
         Scanner scanner = new Scanner(System.in);
 
+        double number1;
+        double number2;
+        char operator;
         double result = 0;
+        boolean validOperator = true;
 
-        System.out.println("Calculator");
-        System.out.println("---------------");
+        System.out.println("Java Calculator App");
+        System.out.println("-------------------");
 
         System.out.println("Enter an operator (+, -, *, /): ");
-        char operator = scanner.nextLine().charAt(0);
+        operator = scanner.nextLine().charAt(0);
 
         System.out.println("Enter a first number: ");
-        double number1 = Double.parseDouble(scanner.nextLine());
+        number1 = Double.parseDouble(scanner.nextLine());
 
         System.out.println("Enter a second number: ");
-        double number2 = Double.parseDouble(scanner.nextLine());
+        number2 = Double.parseDouble(scanner.nextLine());
 
         switch (operator) {
             case '+' -> result = number1 + number2;
             case '-' -> result = number1 - number2;
             case '*' -> result = number1 * number2;
-            case '/' -> result = number1 / number2;
-            default -> System.out.println("Invalid operator");
+            case '/' -> {
+                if (number2 == 0) {
+                    System.out.println("Cannot divide by zero");
+                    validOperator = false;
+                } else {
+                    result = number1 / number2;
+                }
+            }
+
+            default -> {
+                System.out.println("Invalid operator");
+                validOperator = false;
+            }
         }
-        System.out.printf("The result is %.2f ", result);
+        if (validOperator) {
+            System.out.printf("The result is %.2f ", result);
+        }
 
         scanner.close();
     }
