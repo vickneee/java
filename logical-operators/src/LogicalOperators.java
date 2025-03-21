6aac26229cfdf61de60100a0495548bd11d55ae5
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2025-03-21 16:58:46 +0200

    Initial commit

---
+++ logical-operators/src/LogicalOperators.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class LogicalOperators {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        // ! - NOT : Reverses the boolean result of the condition.
+        // && - AND : Both conditions must be true for the if statement to execute.
+        // || - OR : At least one condition must be true for the if statement to execute.
+
+        double temp;
+        boolean isSunny = true;
+
+        System.out.println("Enter the temperature: ");
+        temp = Double.parseDouble(scanner.nextLine());
+
+        if (temp >=15 && temp <= 35 && isSunny) {
+            System.out.println("It is warm and sunny");
+        } else if (temp < 15 && !isSunny) {
+            System.out.println("It is cold and not sunny");
+        } else {
+            System.out.println("The weather is not warm and sunny");
+        }
+
+        scanner.close();
+    }
+}
