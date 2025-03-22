62baa1e152364226d169372c0fe333ebb7e6f7c2
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2025-03-22 16:30:26 +0200

    Initial commit

--- arrays/src/SearchFromArray.java
+++ arrays/src/SearchFromArray.java
@@ -13,6 +13,8 @@ public class SearchFromArray {
         String fruitToSearch;
         // Search for a number in the array
         int numberToSearch = 3;
+        // Flag to check if the item is found
+        boolean isFound = false;
 
         System.out.print("Enter a fruit to search: ");
         fruitToSearch = scanner.nextLine();
@@ -20,6 +22,7 @@ public class SearchFromArray {
 
         for (int i = 0; i < numbers.length; i++) {
             if (numberToSearch == numbers[i]) {
+                isFound = true;
                 System.out.println("Found " + numberToSearch + " at index " + i);
                 break;
             }
@@ -27,18 +30,35 @@ public class SearchFromArray {
 
         for (String fruit : fruits) {
             if (fruit.equals(fruitToSearch)) {
+                isFound = true;
                 System.out.println("Found " + fruitToSearch);
                 break;
             }
         }
+
+        if (!isFound) {
+            System.out.println("Item not found");
+        } else {
+            System.out.println("Item found");
+        }
     }
 
     private static void fruitToSearch(String[] fruits, String fruitToSearch) {
+
+        boolean isFound = false;
+
         for (int i = 0; i < fruits.length; i++) {
             if (fruitToSearch.equals(fruits[i])) {
-                System.out.println("Found " + fruitToSearch + " at index " + i);
+                isFound = true;
+                // System.out.println("Found " + fruitToSearch + " at index " + i);
                 break;
             }
+        }
+
+        if (isFound) {
+            System.out.println("Found " + fruitToSearch + " in the array");
+        } else {
+            System.out.println("Item is not found");
         }
     }
 }
