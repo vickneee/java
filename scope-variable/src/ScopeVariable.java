e68c9e2605cf818b0b38fc58a4e88366aad03cd6
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2025-03-22 13:39:49 +0200

    Initial commit

--- scope-variable/src/ScopeVariable.java
+++ scope-variable/src/ScopeVariable.java
@@ -2,6 +2,7 @@ public class ScopeVariable {
 
     static int y = 3; // CLASS VARIABLE
     static int x = 4; // CLASS VARIABLE NOT USED IN THIS EXAMPLE
+    static double PI = 3.14159; // CLASS VARIABLE CONSTANT
 
     public static void main(String[] args) {
 
@@ -9,16 +10,20 @@ public class ScopeVariable {
 
         System.out.println(x); // 1 (LOCAL VARIABLE x used because it is in the same scope)
 
-        System.out.println(y); // 3 (CLASS VARIABLE y)
+        System.out.println("Class variable " + y); // 3 (CLASS VARIABLE y)
+
+        System.out.println("Constant " + PI); // 3.14159 (CLASS VARIABLE CONSTANT PI)
 
         doSomething();
     }
 
     static void doSomething() {
 
-        int x = 2; // LOCAL VARIABLE (DIFFERENT FROM THE LOCAL VARIABLE x IN THE MAIN METHOD SCOPE
+        int x = 2; // LOCAL VARIABLE (DIFFERENT FROM THE LOCAL VARIABLE x IN THE MAIN METHOD SCOPE)
 
         System.out.println(x); // 2
+
+        System.out.println("Class variable " + y); // 3 (CLASS VARIABLE y)
     }
 
 }
