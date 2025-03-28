3f727394b0081926d92768ded78d25bc2e06714e
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2025-03-28 14:20:57 +0200

    Initial commit

---
+++ getters-and-setters/src/Car.java
@@ -0,0 +1,52 @@
+public class Car {
+
+//    // Variables before encapsulation
+//    String model;
+//    String color;
+//    int year;
+//    double price;
+
+    // Variables after encapsulation
+    private String model;
+    private String color;
+    private int year;
+    private double price;
+
+    // Constructor
+    Car(String model, String color, int year, double price) {
+        this.model = model;
+        this.color = color;
+        this.year = year;
+        this.price = price;
+    }
+
+    // GETTERS make the variables read-only
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getPrice() {
+        return "€" + price;
+    }
+
+    // SETTERS make the variables write-only
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
--- getters-and-setters/src/Main.java
+++ getters-and-setters/src/Main.java
@@ -7,6 +7,32 @@ public class Main {
         // GETTERS = methods that get a value
         // SETTERS = methods that set a value
 
+        Car car = new Car("Chevy", "Black", 2020, 23000.00);
+
+        // Before using getters and setters
+        // car.model = "Ford"; // Can change the value of the variable directly
+
+        // Before using getters and setters
+        // System.out.println("Car model: " + car.model);
+        // System.out.println("Car color: " + car.color);
+        // System.out.println("Car year: " + car.year);
+        // System.out.println("Car price: " + car.price);
+
+        // After using getters and setters
+        System.out.println("Car model: " + car.getModel());
+        System.out.println("Car color: " + car.getColor());
+        System.out.println("Car year: " + car.getYear());
+        System.out.println("Car price: " + car.getPrice());
+
+        // After using getters and setters
+        car.setColor("Red");
+        car.setYear(2021);
+        car.setPrice(25000.00);
+
+        System.out.println("Car model: " + car.getModel());
+        System.out.println("Car color: " + car.getColor());
+        System.out.println("Car year: " + car.getYear());
+        System.out.println("Car price: " + car.getPrice());
 
     }
 }
