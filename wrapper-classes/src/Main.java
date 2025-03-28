89a84f1536feaea673bcc4162346cd316e0e7cb8
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2025-03-28 20:00:38 +0200

    Initial commit

--- wrapper-classes/src/Main.java
+++ wrapper-classes/src/Main.java
@@ -10,26 +10,37 @@ public class Main {
         //                    you need an object for some reason
         //                    (e.g. Collections, generics, etc.)
 
-        // Primitive data types
-        Integer a = new Integer(123);
-        Double d = new Double(123.45);
-        Character f = new Character('a');
-        Boolean h = new Boolean(true);
+//        // Primitive data types
+//        Integer a = new Integer(123);
+//        Double d = new Double(123.45);
+//        Character f = new Character('a');
+//        Boolean h = new Boolean(true);
 
-        // Autoboxing = converting a primitive data type into an object
-        Integer b = 123; // Autoboxing
-        Double e = 123.45; // Autoboxing
-        Character g = 'a'; // Autoboxing
-        Boolean i = true; // Autoboxing
+//        // Autoboxing = converting a primitive data type into an object
+//        Integer b = 123; // Autoboxing
+//        Double e = 123.45; // Autoboxing
+//        Character g = 'a'; // Autoboxing
+//        Boolean i = true; // Autoboxing
 
-        // Unboxing = converting an object to a primitive data type
-        int x = b; // Unboxing
-        double y = e; // Unboxing
-        char z = g; // Unboxing
-        boolean j = i; // Unboxing
+//        // Unboxing = converting an object to a primitive data type
+//        int x = b; // Unboxing
+//        double y = e; // Unboxing
+//        char z = g; // Unboxing
+//        boolean j = i; // Unboxing
 
-        String m = "Pizza";
-        System.out.println(m);
+        String a = Integer.toString(123);
+        String b = Double.toString(123.45);
+        String c = Character.toString('a');
+        String d = Boolean.toString(true);
+
+        String x = a + b + c + d;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+
+        System.out.println(x);
 
     }
 }
