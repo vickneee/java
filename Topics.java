1e54dbe45503bc7f203984046bda90250e0a6d1a
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2025-03-28 16:59:10 +0200

    Initial commit

---
+++ Topics.java
@@ -0,0 +1,27 @@
+public class Topics {
+
+    Variables and Data Types;
+    User Input;
+    Arithmetic Operators;
+    If Statements;
+    Switch Statements;
+    For Loops;
+    While Loops;
+    Do-While Loops;
+    Break and Continue;
+    Arrays;
+    Methods;
+    Classes and Objects;
+    Constructors;
+    Inheritance;
+    Polymorphism;
+    Encapsulation;
+    Abstract Classes;
+    Interfaces;
+    Packages;
+    Exceptions;
+    File I/O;
+    Java Collections;
+    Java Generics;
+    Java Multithreading;
+}
--- arithmetic/src/ArithmeticOperators.java
+++ arithmetic/src/ArithmeticOperators.java
@@ -1,16 +1,21 @@
 public class ArithmeticOperators {
 
     public static void main(String[] args) {
+
+        // Variables
         int x = 10;
         // int x = 10;
         double y = 3;
         double z;
 
+        // Arithmetic Operators
         // z = x - y;
         // z = x + y;
         // z = x * y;
         // z = x % y;
         z = x / y;
+
+        System.out.println(z + " + " + x + " + " + y + " = " + (z + x + y));
 
         System.out.println(z);
     }
