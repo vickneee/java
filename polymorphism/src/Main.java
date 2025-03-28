fdce01760fb5085436fe1de1c92b740537a86069
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2025-03-28 13:23:05 +0200

    Initial commit

--- polymorphism/src/Main.java
+++ polymorphism/src/Main.java
@@ -12,8 +12,18 @@ public class Main {
         Boat boat = new Boat();
         Bike bike = new Bike();
 
-        car.go();
-        boat.go();
-        bike.go();
+//        car.go();
+//        boat.go();
+//        bike.go();
+
+//        Car[] cars = {car , boat, bike};
+//        Boat[] boats = {car , boat, bike};
+//        Bike[] bikes = {car , boat, bike};
+
+        Vehicle[] vehicles = {car, boat, bike};
+
+        for (Vehicle vehicle : vehicles) {
+            vehicle.go();
+        }
     }
 }
