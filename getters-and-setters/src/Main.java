d71c9cc84b323945ebbae3babe5da706a5c03e3b
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2025-03-28 13:49:03 +0200

    Initial commit

---
+++ getters-and-setters/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Getters and Setters = a way to encapsulate and protect your data
+        //                       from accidental corruption.
+        // GETTERS = methods that get a value
+        // SETTERS = methods that set a value
+
+
+    }
+}
